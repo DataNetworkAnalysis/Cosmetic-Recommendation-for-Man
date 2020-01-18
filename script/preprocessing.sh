58a2d27ebe329ae795c4c67c13974f2d5c56bda5
TooTouch <wogur379@gmail.com>
2020-01-18 15:22:28 +0900

    preprocess bash

---
+++ script/preprocessing.sh
@@ -0,0 +1,4 @@
+python -u ../code/main.py --train --wordpath='../saved_file/replace_word.json' --pospath='../saved_file/pos_dict.json' && \
+python -u ../code/main.py --train --wordpath='../saved_file/replace_word.json' && \
+python -u ../code/main.py --train  --pospath='../saved_file/pos_dict.json' && \
+python -u ../code/main.py --train 
