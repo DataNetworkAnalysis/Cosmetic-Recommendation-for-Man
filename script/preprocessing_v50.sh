0f5b193d40500ea497038c779afd321342a81137
TooTouch <wogur379@gmail.com>
2020-01-18 21:38:33 +0900

    preprocessing embed size 50

---
+++ script/preprocessing_v50.sh
@@ -0,0 +1,4 @@
+python -u ../code/main.py --train --wordpath='../saved_file/replace_word.json' --pospath='../saved_file/pos_dict.json' --embed_size=50 && \
+python -u ../code/main.py --train --wordpath='../saved_file/replace_word.json' --embed_size=50 && \
+python -u ../code/main.py --train --pospath='../saved_file/pos_dict.json' --embed_size=50 && \
+python -u ../code/main.py --train --embed_size=50 
