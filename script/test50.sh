871bb107701f12dd9958a5923e59ec4b05aee151
TooTouch <wogur379@gmail.com>
2020-01-25 00:01:47 +0900

    delete trash

--- script/test50.sh
+++
@@ -1,5 +0,0 @@
-python -u ../code/main.py --search="가성비 좋은 왁스" --embed_size=50 && \
-python -u ../code/main.py --search="가성비 좋은 왁스" --wordpath='../saved_file/replace_word.json'  --embed_size=50 && \
-python -u ../code/main.py --search="가성비 좋은 왁스" --pospath='../saved_file/pos_dict.json'  --embed_size=50 && \
-python -u ../code/main.py --search="가성비 좋은 왁스" --wordpath='../saved_file/replace_word.json' --pospath='../saved_file/pos_dict.json' --embed_size=50
-
